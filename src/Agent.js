3f505ef695b41c562ed7d765d05153287f70db97
Carlos Romero Brox <brox@tid.es>
2012-11-30 11:54:56 +0100

    remove heading spaces at header

--- src/Agent.js
+++ src/Agent.js
@@ -4,9 +4,9 @@
  This file is part of PopBox.
 
  PopBox is free software: you can redistribute it and/or modify it under the
-  terms of the GNU Affero General Public License as published by the Free
-  Software Foundation, either version 3 of the License, or (at your option)
-  any later version.
+ terms of the GNU Affero General Public License as published by the Free
+ Software Foundation, either version 3 of the License, or (at your option) any
+ later version.
  PopBox is distributed in the hope that it will be useful, but WITHOUT ANY
  WARRANTY; without even the implied warranty of MERCHANTABILITY or
  FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public
